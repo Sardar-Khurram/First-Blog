4e1c5a703f42e56a12a2f067aa39a409f6b260ce
Sardar-Khurram <sardarkhurram121@gmail.com>
2025-04-09 09:04:08 +0500

    Add file for google console

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+// next-sitemap.config.js
+
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://your-site.vercel.app', // ✅ Replace with your actual domain
+    generateRobotsTxt: true,
+  };
+  
